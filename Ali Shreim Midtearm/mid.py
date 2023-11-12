d23890e23084d5a00039fe507b15952dabb033eb
AliShreim1 <ali.shreim@gmail.com>
2023-11-12 10:29:37 +0200

    adding the function that disply the html content of a page

--- Ali Shreim Midtearm/mid.py
+++ Ali Shreim Midtearm/mid.py
@@ -1,8 +1,29 @@
+import requests
+from bs4 import BeautifulSoup# learned from youtube (https://youtu.be/0uiPOxUcLgg?feature=shared)
+
 def addTab(dictionary:dict):
     title=input('Enter the title of the new tab')
     url=input('Enter the url of the page')
     dictionary[title]=url
 
-def closeTab(list:list,index:int):
-    list.pop(index-1)
+def closeTab(list:list,index):
+    if(index>len(list) or isinstance(index,int)):
+        list.pop()
+    else:
+        list.pop(index-1)
+
+def displyContent(list:list,dictionary:dict,index):
+    tab=None
+    if(index>len(list) or isinstance(index,int)):
+        tab=list[len(list)-1]
+    else:
+        tab=list[index-1]
+    url=dictionary[tab]
+    content=requests.get(url)
+    print(content.text)
+
+
+
+
+
 
