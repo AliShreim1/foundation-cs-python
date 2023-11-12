7d83641559b06a0a1de9cde8f10df41071138688
AliShreim1 <ali.shreim@gmail.com>
2023-11-12 09:51:05 +0200

    adding the close tab function

--- Ali Shreim Midtearm/mid.py
+++ Ali Shreim Midtearm/mid.py
@@ -2,3 +2,7 @@ def addTab(dictionary:dict):
     title=input('Enter the title of the new tab')
     url=input('Enter the url of the page')
     dictionary[title]=url
+
+def closeTab(list:list,index:int):
+    list.pop(index-1)
+
