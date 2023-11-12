a5a372910dbfa073213d125b21dfa6cd68af4aa7
AliShreim1 <ali.shreim@gmail.com>
2023-11-12 13:13:49 +0200

    finishing the exam

--- Ali Shreim Midtearm/mid.py
+++ Ali Shreim Midtearm/mid.py
@@ -2,10 +2,11 @@ import requests
 from bs4 import BeautifulSoup# learned from youtube (https://youtu.be/0uiPOxUcLgg?feature=shared)
 import json
 
-def addTab(dictionary:dict):
+def addTab(dictionary:dict,list:list):
     title=input('Enter the title of the new tab')
     url=input('Enter the url of the page')
     dictionary[title]=url
+    list.append(title)
 
 def closeTab(list:list,index):
     if(index>len(list) or isinstance(index,int)):
@@ -66,3 +67,46 @@ def saveAsJsonFile(filepath,dictionary,openTabs):
             json.dump(i,jfile)
     print('save is done')
 
+def stop():
+    exit()
+
+
+openTabs=[]
+tabs={}
+
+while(True):
+    print("Menu:")
+    print("1. Open Tab")
+    print("2. Close Tab")
+    print("3. Switch Tab")
+    print("4. Display All Tabs")
+    print("5. Open Nested Tab")
+    print("6. Clear All Tabs")
+    print("7. Save Tabs")
+    print("8. Import Tabs")
+    print("9. Exit")
+    choice=input('Enter a number \n')
+    if choice == "1":
+        addTab(tabs,openTabs)
+    elif choice == "2":
+        index=int(input('enter the index'))
+        closeTab(openTabs,index)
+    elif choice == "3":
+        index=int(input('enter the index'))
+        displyContent(openTabs,tabs,index)
+    elif choice == "4":
+        printTheTitles(openTabs)
+    elif choice == "5":
+        index=int(input('enter the index'))
+        createNestedTabs(tabs,index)
+    elif choice == "6":
+        clearOpenTabs(openTabs)
+    elif choice == "7":
+        path = input("Enter file path to save tabs: ")
+        saveAsJsonFile(path,tabs,openTabs)
+    elif choice == "8":
+        print('.')
+    elif choice == "9":
+        stop()
+    else:
+        print("Invalid choice. Please enter a number from 1 to 9.")
